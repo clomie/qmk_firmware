61b99bcf2bd7924171ddea8c4f516cedf9b349ae
clomie <cclomie@gmail.com>
2019-05-26 19:42:33 +0900

    左手1列目3行目のキーにCtrlをアサイン

--- keyboards/ergodash/rev2/keymaps/clomie/keymap.c
+++ keyboards/ergodash/rev2/keymaps/clomie/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+--------+--------+--------+--------+--------+--------+------------------------+--------+--------+--------+--------+--------+--------+--------|
    * | Tab  |    Q   |    W   |    E   |    R   |    T   |   [{   |                        |   ]}   |    Y   |    U   |    I   |    O   |    P   |   @`   |
    * |------+--------+--------+--------+--------+--------+--------+------------------------+--------+--------+--------+--------+--------+--------+--------|
-   * |      |    A   |    S   |    D   |    F   |    G   |   \|   |                        |    B   |    H   |    J   |    K   |    L   |   ;+   |   :*   |
+   * | Ctrl |    A   |    S   |    D   |    F   |    G   |   \|   |                        |    B   |    H   |    J   |    K   |    L   |   ;+   |   :*   |
    * |------+--------+--------+--------+--------+--------+---------------------------------+--------+--------+--------+--------+--------+--------+--------|
    * | Shift|    Z   |    X   |    C   |    V   |    B   | PrtScr |                        | Backsp |    N   |    M   |   ,>   |   .>   |   /?   |   \_   |
    * |---------------+--------+--------+--------+--------+--------+--------+------+--------+--------+--------+--------+--------+--------+-----------------|
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAC] = LAYOUT( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_CIRC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_LBRC,                          JP_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   \
-    XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_YEN,                           KC_B,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_YEN,                           KC_B,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PSCR,                          KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS, \
     KC_LCTL, KC_LALT, KC_LGUI, KC_LALT,          MACLOWER,LSHFTSP, KC_LSFT,        KC_ENT,  RSHFTSP, MACRAISE,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+--------+--------+--------+--------+--------+--------+------------------------+--------+--------+--------+--------+--------+--------+--------|
    * | Tab  |    Q   |    W   |    E   |    R   |    T   |   [{   |                        |   ]}   |    Y   |    U   |    I   |    O   |    P   |   @`   |
    * |------+--------+--------+--------+--------+--------+--------+------------------------+--------+--------+--------+--------+--------+--------+--------|
-   * |      |    A   |    S   |    D   |    F   |    G   |   \|   |                        |    B   |    H   |    J   |    K   |    L   |   ;+   |   :*   |
+   * | Ctrl |    A   |    S   |    D   |    F   |    G   |   \|   |                        |    B   |    H   |    J   |    K   |    L   |   ;+   |   :*   |
    * |------+--------+--------+--------+--------+--------+---------------------------------+--------+--------+--------+--------+--------+--------+--------|
    * | Shift|    Z   |    X   |    C   |    V   |    B   | PrtScr |                        | Backsp |    N   |    M   |   ,>   |   .>   |   /?   |   \_   |
    * |---------------+--------+--------+--------+--------+--------+--------+------+--------+--------+--------+--------+--------+--------+-----------------|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_WIN] = LAYOUT( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_CIRC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_LBRC,                          JP_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   \
-    XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_YEN,                           KC_B,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_YEN,                           KC_B,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PSCR,                          KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS, \
     KC_LCTL, KC_LWIN, KC_LCTL, KC_LALT,          WINLOWER,LSHFTSP, KC_LSFT,        KC_ENT,  RSHFTSP, WINRAISE,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
