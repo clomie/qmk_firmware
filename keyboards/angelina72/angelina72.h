874dbd2ed8549c1d95a852c0f6a3df512ed227f3
clomie <cclomie@gmail.com>
2019-08-20 16:20:33 +0900

    Add angelina72 keyboard

---
+++ keyboards/angelina72/angelina72.h
@@ -0,0 +1,47 @@
+/* Copyright 2019 clomie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "quantum.h"
+
+/* This a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the leys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+    L00, L01, L02, L03, L04, L05, L06,     R00, R01, R02, R03, R04, R05, R06, R07, \
+    L10, L11, L12, L13, L14, L15,               R10, R11, R12, R13, R14, R15, R16, \
+    L20, L21, L22, L23, L24, L25,               R20, R21, R22, R23, R24, R25, R26, \
+    L30, L31, L32, L33, L34, L35, L36,     R30, R31, R32, R33, R34, R35, R36, R37, \
+    L40, L41, L42, L43,      L44, L45,     R40, R41,      R42, R43, R44, R45       \
+) \
+{ \
+    { L06,   L05,   L04,   L03,   L02,   L01,   L00,   KC_NO }, \
+    { KC_NO, L15,   L14,   L13,   L12,   L11,   L10,   KC_NO }, \
+    { KC_NO, L25,   L24,   L23,   L22,   L21,   L20,   KC_NO }, \
+    { L36,   L35,   L34,   L33,   L32,   L31,   L30,   KC_NO }, \
+    { L45,   L44,   KC_NO, L43,   L42,   L41,   L40,   KC_NO }, \
+\
+    { R00,   R01,   R02,   R03,   R04,   R05,   R06,   R07   }, \
+    { KC_NO, R10,   R11,   R12,   R13,   R14,   R15,   R16   }, \
+    { KC_NO, R20,   R21,   R22,   R23,   R24,   R25,   R26   }, \
+    { R30,   R31,   R32,   R33,   R34,   R35,   R36,   R37   }, \
+    { R40,   R41,   KC_NO, R42,   R43,   R44,   R45,   KC_NO }  \
+}
---
+++ keyboards/angelina72/keymaps/default/keymap.c
@@ -0,0 +1,123 @@
+/* Copyright 2019 clomie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "keymap_jp.h"
+
+extern keymap_config_t keymap_config;
+
+enum layer_names {
+  _MAC,
+  _WIN,
+  _RAISE,
+  _LOWER,
+  _ADJUST,
+};
+
+enum custom_keycodes {
+  MAC = SAFE_RANGE,
+  WIN,
+};
+#define SPRAISE LT(_RAISE,  KC_SPACE)
+#define SPLOWER LT(_LOWER,  KC_SPACE)
+#define SPADJST LT(_ADJUST, KC_SPACE)
+
+#define MACZKHK RCMD(KC_SPACE)
+#define CTLPGUP LCTL(KC_PGUP)
+#define CTLPGDN LCTL(KC_PGDN)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_MAC] = LAYOUT( \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_CIRC, JP_YEN,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, \
+    MACZKHK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, JP_RBRC, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PSCR,        KC_DEL,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS, \
+    KC_LCTL, KC_LALT, KC_LCMD, JP_MEISU,         SPLOWER, KC_BSPC,        KC_ENT,  SPRAISE,          JP_MKANA,KC_RCMD, KC_RALT, KC_LSFT  \
+  ),
+
+  [_WIN] = LAYOUT( \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_CIRC, JP_YEN,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, \
+    KC_ZKHK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, JP_RBRC, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PSCR,        KC_DEL,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS, \
+    KC_LCTL, KC_LWIN, KC_LALT, JP_MHEN,          SPLOWER, KC_BSPC,        KC_ENT,  SPRAISE,          JP_HENK, KC_RWIN, KC_RALT, KC_APP   \
+  ),
+
+  [_RAISE] = LAYOUT( \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, \
+    _______, _______, _______, _______, _______, _______,                          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  CTLPGUP, XXXXXXX, \
+    _______, _______, _______, _______, _______, _______,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, CTLPGDN, XXXXXXX, \
+    _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______,          SPADJST, _______,        _______, _______,          _______, _______, _______, _______ \
+  ),
+
+  [_LOWER] = LAYOUT( \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, \
+    _______, _______, _______, _______, _______, _______,                          _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,        _______, _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, \
+    _______, _______, _______, _______,          _______, _______,        _______, SPADJST,          KC_0,    KC_DOT,  _______, _______ \
+  ),
+
+  [_ADJUST] = LAYOUT( \
+    MAC,     _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, WIN,     \
+    _______, _______, _______, _______, RESET,   _______,                          RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, _______, _______, \
+    _______, _______, _______, DEBUG,   _______, _______,                          RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______,          _______, _______,        _______, _______,          _______, _______, _______, _______ \
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  dprintf("process_record_user: keycode = %5u"
+          ", event.pressed = %u"
+          ", event.time = %7u"
+          ", tap.count = %2u"
+          ", tap.interrupted = %u\n"
+              , keycode
+              , record->event.pressed
+              , record->event.time
+              , record->tap.count
+              , record->tap.interrupted);
+
+  switch (keycode) {
+    case MAC:
+      if (record->event.pressed) {
+        dprintln("Enable MAC mode");
+        set_single_persistent_default_layer(_MAC);
+      }
+      break;
+    case WIN:
+      if (record->event.pressed) {
+        dprintln("Enable WIN mode");
+        set_single_persistent_default_layer(_WIN);
+      }
+      break;
+  }
+  return true;
+}
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
